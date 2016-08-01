bc1e34431b5607bfd1389dcae12b3765314bf565
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-31 23:14:50 -0300

    FoodMachine Class added.

---
+++ src/FoodMachine.rb
@@ -0,0 +1,6 @@
+=begin
+	*
+	* A food machine has products of three types, A, B and C, which are respectively worth
+	* $270, $340 and $390. The machine accepts and gives of returned $10, $50 and $100 coins.
+	*	
+=end
